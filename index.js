8e7f000bb42576c8475e71ccc0d3d8287f8c1e2c
Aguleo <ifeanyileo42@gmail.com>
2024-04-19 12:35:38 +0100

    created css file

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let a;
 let b;
+let c;
+
+
 
 // change of branch in git-bash
 // git branch -M main ------ this will change from (master) to (main)
+// git init
+// git add .
+// git commit -m "my first commit"
+// creating a new branch (git checkout -b new-branch)
+// git log 
+// git status
